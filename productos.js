3f9e2ed1733d7103743408f3977c788949f3ddb1
Moe <35382301+Moewagon@users.noreply.github.com>
2023-05-19 03:47:03 +0200

    Intento de añadir funcion para borrar div pero no

--- productos.js
+++ productos.js
@@ -1,112 +1,94 @@
-/* $(document).ready(function () {
-  $.getJSON("productos.json", function (data) {
-    var items = [];
-    $.each(data, function (key, val) {
-      items.push("<li class='list-group-item'>" + val.nombre + "<span class='precio'>$" + val.precio.toFixed(2) + "</span><input class='texto price-producto form-control' type='number' value='0'></li>");
-    });
-    $("<ul/>", {
-      class: "list-group",
-      html: items.join("")
-    }).appendTo("#productos-lista");
-
-    // Calcula el total al cambiar la cantidad o el descuento
-    $(".texto").change(function () {
-      var subtotal = 0;
-      var descuento = $("#descuento").val() || 0;
-      $("#productos-lista li").each(function () {
-        var precio = parseFloat($(this).find(".precio").text().replace("$", ""));
-        var cantidad = parseInt($(this).find(".texto").val());
-        subtotal += precio * cantidad;
-      });
-      var total = subtotal * (100 - descuento) / 100;
-      $("#total").text("Total: $" + total.toFixed(2));
-    });
-
-    $("#descuento").change(function () {
-      $(".texto").trigger("change");
-    });
-  });
-});
- */
-
 async function getProductos() {
-
-  let listaProductos = await fetch("./productos.json")
-  let productos = await listaProductos.json()
-  const listado = document.getElementById("productos-lista")
+  let listaProductos = await fetch("./productos.json");
+  let productos = await listaProductos.json();
+  const listado = document.getElementById("productos-lista");
 
   productos.forEach((producto) => {
-
-    let itemList = document.createElement("div")
-    listado.appendChild(itemList)
-    itemList.classList.add('list-group-item')
-    itemList.innerHTML = `<span class='precio'>${producto.nombre} $<span class='numero'>${producto.precio.toFixed(2)}</span></span><input class='texto price-producto form-control' data-nombre='${producto.nombre}' data-precio=${producto.precio} type='number' value='0'>`
-
-
-  })
+    let itemList = document.createElement("div");
+    listado.appendChild(itemList);
+    itemList.classList.add("list-group-item");
+    itemList.innerHTML = `<span class='precio'>${producto.nombre} $<span class='numero'>${producto.precio.toFixed(
+      2
+    )}</span></span><input class='texto price-producto form-control' data-nombre='${
+      producto.nombre
+    }' data-precio=${producto.precio} type='number' value='0'>`;
+  });
 }
-async function getCombos() {
 
-  let listaCombos = await fetch("./combos.json")
-  let combos = await listaCombos.json()
-  const listado = document.getElementById("combos-lista")
+async function getCombos() {
+  let listaCombos = await fetch("./combos.json");
+  let combos = await listaCombos.json();
+  const listado = document.getElementById("combos-lista");
 
   combos.forEach((producto) => {
-
-    let itemList = document.createElement("div")
-    listado.appendChild(itemList)
-    itemList.classList.add('list-group-item')
-    itemList.innerHTML = `<span class='precio'>${producto.nombre} $<span class='numero'>${producto.precio.toFixed(2)}</span></span><input class='texto price-producto form-control' data-nombre='${producto.nombre}' data-precio=${producto.precio} type='number' value='0'>`
-
-  })
+    let itemList = document.createElement("div");
+    listado.appendChild(itemList);
+    itemList.classList.add("list-group-item");
+    itemList.innerHTML = `<span class='precio'>${producto.nombre} $<span class='numero'>${producto.precio.toFixed(
+      2
+    )}</span></span><input class='texto price-producto form-control' data-nombre='${
+      producto.nombre
+    }' data-precio=${producto.precio} type='number' value='0'>`;
+  });
 }
 
 window.onload = async () => {
-
-  await getProductos()
-  await getCombos()
+  await getProductos();
+  await getCombos();
 
   document.getElementById("calcular").addEventListener("click", (e) => {
+    let pedido = [];
+    let precioTotal = document.getElementById("precio-total");
+    let descuento = document.getElementById("descuento").value;
+    let listaProductos = document.getElementsByClassName("price-producto");
+    let listaPedidos = document.getElementById("lista-pedidos");
+    let total = 0;
 
-    let pedido = []
-    let precioTotal = document.getElementById("precio-total")
-    let descuento = document.getElementById("descuento").value
-    let listaProductos = document.getElementsByClassName("price-producto")
-    let listaPedidos = document.getElementById("lista-pedidos")
-    let total = 0
     for (let i = 0; i < listaProductos.length; i++) {
-
       if (!descuento && listaProductos[i].value != 0) {
-
-        pedido.push({ nombre: listaProductos[i].dataset.nombre, precio: listaProductos[i].dataset.precio, cantidad: listaProductos[i].value })
-        total += listaProductos[i].dataset.precio * listaProductos[i].value
-
+        pedido.push({
+          nombre: listaProductos[i].dataset.nombre,
+          precio: listaProductos[i].dataset.precio,
+          cantidad: listaProductos[i].value,
+        });
+        total += listaProductos[i].dataset.precio * listaProductos[i].value;
       }
 
       if (descuento && listaProductos[i].value != 0) {
-        let precioDescontado = (listaProductos[i].dataset.precio * listaProductos[i].value) * (descuento / 100)
-        pedido.push({ nombre: listaProductos[i].dataset.nombre, precio: listaProductos[i].dataset.precio, cantidad: listaProductos[i].value })
-        total += listaProductos[i].dataset.precio * listaProductos[i].value - precioDescontado
-
+        let precioDescontado =
+          (listaProductos[i].dataset.precio * listaProductos[i].value * descuento) / 100;
+        pedido.push({
+          nombre: listaProductos[i].dataset.nombre,
+          precio: listaProductos[i].dataset.precio,
+          cantidad: listaProductos[i].value,
+        });
+        total +=
+          listaProductos[i].dataset.precio * listaProductos[i].value - precioDescontado;
       }
-      listaProductos[i].value = 0
+      listaProductos[i].value = 0;
     }
-    let divPedido = document.createElement("div")
-    divPedido.classList.add("pedido-card")
 
-    pedido.forEach(producto => {
-      divPedido.innerHTML += `<p>${producto.cantidad}x - ${producto.nombre} - ${producto.precio} $<p>`
-    })
-    divPedido.innerHTML += `<p>Total del pedido: ${total} $ <br><br><button class="btn"><i class="bi bi-trash"></i></button></i><p>`
-    listaPedidos.appendChild(divPedido)
+    let divPedido = document.createElement("div");
+    divPedido.classList.add("pedido-card");
 
-    let ingresosPedidos = parseInt(document.getElementById("total-pedidos").textContent)
+    pedido.forEach((producto) => {
+      divPedido.innerHTML += `<p>${producto.cantidad}x - ${producto.nombre} - ${producto.precio} $<p>`;
+    });
 
-    ingresosPedidos += total
+    divPedido.innerHTML += `<p>Total del pedido: ${total} $<br><br><button class="botonborrar" onclick="eliminarDiv(this)"><i class="bi bi-trash"></i></button></i><p>`;
+    listaPedidos.appendChild(divPedido);
 
-    document.getElementById("total-pedidos").textContent = ingresosPedidos
+    let ingresosPedidos = parseInt(document.getElementById("total-pedidos").textContent);
 
-    precioTotal.textContent = `PRECIO TOTAL = $ ${total.toFixed(2)}`
-  })
+    ingresosPedidos += total;
 
-}
+    document.getElementById("total-pedidos").textContent = ingresosPedidos;
+
+    precioTotal.textContent = `PRECIO TOTAL = $ ${total.toFixed(2)}`;
+  });
+
+  function eliminarDiv(boton) {
+    var divPadre = boton.parentNode;
+    divPadre.parentNode.removeChild(divPadre);
+  }
+};
